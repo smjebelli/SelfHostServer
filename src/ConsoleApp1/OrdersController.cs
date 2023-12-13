eb23ea6ec25e25bca895b9f5daccfe5d85213aae
m.jebelli <s.m.jebelli@gmail.com>
2023-12-13 11:32:24 +0330

    initial

---
+++ src/ConsoleApp1/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ConsoleApp1
+{
+    public class OrdersController:ApiController
+    {
+        Order[] orders = new Order[]
+        {
+            new Order { Id = 1,Name ="Tomato soup", Category = "Groceries", Price = 1},
+            new Order { Id = 2,Name ="Yo-yo", Category = "Toys", Price = 3.75M},
+            new Order { Id = 3,Name ="Hammer", Category = "Hardware", Price = 16.99M},
+        };
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return orders;
+        }
+
+    }
+
+    public class Order
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+        public string  Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductsCotnroller : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{Id= 1, Name= "p1"},
+            new Product{Id= 2, Name= "p2"},
+            new Product{Id= 3, Name= "p3"},
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Product> All()
+        {
+            return products;
+        }
+    }
+}
---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:5009");
+
+            config.Routes.MapHttpRoute("Default", "api/{controller}/{id}", new {id=RouteParameter.Optional});
+
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            server.OpenAsync().Wait();
+
+            // for waiting
+            Console.ReadLine();
+
+        }
+    }
+}
+
+//  http://localhost:5009/api/orders
+
+// powershell command to open port:
+//    netsh http add urlacl url=http://+:5009/ user=m.jebelli
